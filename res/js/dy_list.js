bdc3e0223e013791eebd8dcaeef0308d806ee70a
HomeEmail <591694604@qq.com>
2016-07-22 17:38:32 +0800

    Signed-off-by: HomeEmail <591694604@qq.com>

--- res/js/dy_list.js
+++ res/js/dy_list.js
@@ -20,9 +20,13 @@ $('.list-page-main').on('scroll',function(e){
 	if((top+wh-46+5)>=$('#listContent').height()){
 		//加在更多
 		if($('.nomore').length>0){
+			$('.nomore').show().html('正在加载...');
 			return;
+		}else{
+			$('#listContentBox').append('<div class="nomore">正在加载...</div>');
 		}
-		$('#listContentBox').append('<div class="nomore">没有更多了!</div>');
+		getMoreData();
+		//$('#listContentBox').append('<div class="nomore">没有更多了!</div>');
 	}
 });
 
@@ -30,11 +34,75 @@ $('.list-page-main').on('scroll',function(e){
 $('.listTypeSelectedBox').on('tap',function(e){
 	$(this).hide();
 	$('.listTypeBox.fixed').show();
+	updateListTypeFixedSwiper();
 });
 
-hideLoading();
+var channelId,pageNo;
+var urlParaObj=getUrlPara();
+channelId=urlParaObj.channelId;
+pageNo=1;
 
-new Swiper('.listType-type',{
+
+
+function getTypeData(){
+	$.ajax({
+	  type: 'GET',
+	  url: serverAddress+'/utvgoClient/interfaces/hdtvContent_typeContext.action',
+	  // data to be added to query string:
+	  data: {channelId:channelId,pageSize:10,pageNo:pageNo,publishYear:'',typeId:'',areaId:''},
+	  // type of data we are expecting in return:
+	  dataType: 'json',
+	  success: function(data){
+	  	hideLoading();
+	    renderTypeData(data);
+	  },
+	  error: function(xhr, type){
+	    alert('network error!');
+	  }
+	});
+}
+function renderTypeData(data){
+	var s='';
+	//type
+	s+='<div id="listType-type" class="listType-row listType-type swiper-container"><div class="swiper-wrapper">';
+
+	s+='</div></div>';
+
+	//area
+	s+='<div id="listType-area" class="listType-row listType-area swiper-container"><div class="swiper-wrapper">';
+
+	s+='</div></div>';
+
+
+	//year
+	s+='<div id="listType-year" class="listType-row listType-year swiper-container"><div class="swiper-wrapper">';
+
+	s+='</div></div>';
+
+	$('.listTypeBox').html(s);
+	
+}
+
+function getMoreData(){
+
+	//加载到内容了
+	//$('.nomore').hide();
+
+	//没有内容了
+	//$('.nomore').show().html('没有更多了!');
+}
+
+function updateListTypeFixedSwiper(){
+	// listTypeType.reInit();
+	// listTypeArea.reInit();
+	// listTypeYear.reInit();
+
+	listTypeTypeFixed.reInit();
+	listTypeAreaFixed.reInit();
+	listTypeYearFixed.reInit();
+}
+
+var listTypeType=new Swiper('#listType-type',{
 	//pagination: '#slideTopBannerIndicator',
 	paginationClickable: true,
 	//slidesPerView: 1,
@@ -46,7 +114,7 @@ new Swiper('.listType-type',{
 	//onSlideChangeEnd:bannerPageChangeTips
 });
 
-new Swiper('.listType-area',{
+var listTypeArea=new Swiper('#listType-area',{
 	//pagination: '#slideTopBannerIndicator',
 	paginationClickable: true,
 	//slidesPerView: 1,
@@ -58,7 +126,42 @@ new Swiper('.listType-area',{
 	//onSlideChangeEnd:bannerPageChangeTips
 });
 
-new Swiper('.listType-year',{
+var listTypeYear=new Swiper('#listType-year',{
+	//pagination: '#slideTopBannerIndicator',
+	paginationClickable: true,
+	//slidesPerView: 1,
+	slidesPerView: 'auto',
+	freeMode: true
+	//loop: true,
+	//autoplay:8000,
+	//onFirstInit:bannerPageChangeTips,
+	//onSlideChangeEnd:bannerPageChangeTips
+});
+var listTypeTypeFixed=new Swiper('#listType-type-fixed',{
+	//pagination: '#slideTopBannerIndicator',
+	paginationClickable: true,
+	//slidesPerView: 1,
+	slidesPerView: 'auto',
+	freeMode: true
+	//loop: true,
+	//autoplay:8000,
+	//onFirstInit:bannerPageChangeTips,
+	//onSlideChangeEnd:bannerPageChangeTips
+});
+
+var listTypeAreaFixed=new Swiper('#listType-area-fixed',{
+	//pagination: '#slideTopBannerIndicator',
+	paginationClickable: true,
+	//slidesPerView: 1,
+	slidesPerView: 'auto',
+	freeMode: true
+	//loop: true,
+	//autoplay:8000,
+	//onFirstInit:bannerPageChangeTips,
+	//onSlideChangeEnd:bannerPageChangeTips
+});
+
+var listTypeYearFixed=new Swiper('#listType-year-fixed',{
 	//pagination: '#slideTopBannerIndicator',
 	paginationClickable: true,
 	//slidesPerView: 1,
